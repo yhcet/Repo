aec63a61497b1822fec698d0df09200ab21721e6
yhcet <yhcet@globalcas.com>
2020-01-29 17:36:49 +0900

    컨트롤러

--- src/main/java/com/kosta/helloworld/StudentRepository.java
+++ src/main/java/com/kosta/helloworld/StudentRepository.java
@@ -1,9 +1,11 @@
 package com.kosta.helloworld;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(path = "students-api", collectionResourceRel = "students-api")
 public interface StudentRepository extends CrudRepository<Student, Long>{
     public List<Student> findByName(String name);
 }
